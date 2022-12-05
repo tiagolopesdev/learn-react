d3d5ddf6131bdc8da03bb5941a12d6f12a0c87dc
Tiago Lopes <saxtiago14@gmail.com>
2022-12-05 19:57:29 -0300

    Feat - settings to use button

--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -12,27 +12,27 @@ export class Home extends Component {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 2
+    postsPerPage: 10
   };
 
   async componentDidMount() {
     await this.loadPosts();
-  }  
-  
+  }
+
   loadPosts = async () => {
     const { page, postsPerPage } = this.state;
 
     const postsAndPhotos = await loadPosts();
-    this.setState({ 
+    this.setState({
       posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos      
+      allPosts: postsAndPhotos
     });
   }
 
   loadMorePosts = () => {
     const {
-      page, 
-      postsPerPage, 
+      page,
+      postsPerPage,
       allPosts,
       posts
     } = this.state;
@@ -43,17 +43,22 @@ export class Home extends Component {
     this.setState({ posts, page: nextPage });
   }
 
-  render () {
-    const { posts } = this.state;
+  render() {
+    const { posts, page, postsPerPage, allPosts } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
 
     return (
       <section className="container">
-        <Posts posts={ posts } />
-        <Button 
-          text="Load more posts"
-          onClick={this.loadMorePosts}
-        />
+        <Posts posts={posts} />
+
+        <div className="button-container">
+          <Button
+            text="Load more posts"
+            onClick={this.loadMorePosts}
+            disabled={noMorePosts}
+          />
+        </div>
       </section>
     );
-  } 
+  }
 }
