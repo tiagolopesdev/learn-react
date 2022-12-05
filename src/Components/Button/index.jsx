05dfda74464d80907f8ef20507203cbc6aa80a10
Tiago Lopes <saxtiago14@gmail.com>
2022-12-05 19:55:18 -0300

    Feat - component button finish

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,10 +1,15 @@
+import "./styles.css";
 import { Component } from "react";
 
-export class Button extends Component {     
+export class Button extends Component {
     render() {
-        const { text, onClick } = this.props;
+        const { text, onClick, disabled } = this.props;
         return (
-            <button onClick={onClick}>
+            <button 
+                className="button"
+                onClick={onClick}
+                disabled={disabled}
+            >
                 { text }
             </button>
         );
